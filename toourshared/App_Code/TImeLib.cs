323929af71504d8c0648dcebc6207a66dc4234c5
PYEONGKEUN <skvudrms54@gmail.com>
2019-10-10 01:52:22 +0900

    Create TImeLib.cs
    DB 프로그래밍시 필요한 시간 라이브러리 생성

---
+++ toourshared/App_Code/TImeLib.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+/// <summary>
+/// TimeLib 요약 설명입니다.
+/// DB의 일시, 일 부분에 사용할 시간 라이브러리
+/// </summary>
+namespace tooushared.App_Code.connector
+{
+    public class TimeLib
+    {
+  
+
+        public TimeLib()
+        {
+
+        }
+              
+
+        public static string GetTimeStamp()
+        {
+            string now = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            return now;
+        }
+        public static string GetToday()
+        {
+            string now = DateTime.Now.ToString("yyyy-MM-dd");
+            return now;
+        }
+    }
+}
