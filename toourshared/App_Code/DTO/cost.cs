4e62f2acad7f2a970b5b9f3b2ebb36613e43e88b
dlskwls32 <dlskwls322@DAUM.NET>
2019-10-16 15:01:15 +0900

    dto

---
+++ toourshared/App_Code/DTO/cost.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// cost의 요약 설명입니다.
+/// </summary>
+public class cost
+{
+    private String cos_no;
+    public cost(string cos_no = null)
+    {
+        this.Cos_no = cos_no;
+
+        //
+        // TODO: 여기에 생성자 논리를 추가합니다.
+        //
+    }
+
+    public string Cos_no { get => cos_no; set => cos_no = value; }
+
+    public override bool Equals(object obj)
+    {
+        return obj is cost cost &&
+               Cos_no == cost.Cos_no;
+    }
+
+    public override int GetHashCode()
+    {
+        return -1221565602 + EqualityComparer<string>.Default.GetHashCode(Cos_no);
+    }
+}   
