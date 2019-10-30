693687e639be21e648d0b40154c6ba902703ba11
PYEONGKEUN <skvudrms54@gmail.com>
2019-10-30 18:18:38 +0900

    윤성이꺼 업데이트

---
+++ adminForm/Report.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Report의 요약 설명입니다.
+/// </summary>
+public class Report
+{
+    private string rep_no;
+    private string trv_no;
+    private string rep_timestap;
+    private string rep_mem_id;
+    private string rep_reason;
+
+    public Report(string rep_no = null, string trv_no = null, string rep_timestap = null, string rep_mem_id = null, string rep_reason = null)
+    {
+        this.rep_no = rep_no;
+        this.trv_no = trv_no;
+        this.rep_timestap = rep_timestap;
+        this.rep_mem_id = rep_mem_id;
+        this.rep_reason = rep_reason;
+    }
+
+    public string Rep_no { get => rep_no; set => rep_no = value; }
+    public string Trv_no { get => trv_no; set => trv_no = value; }
+    public string Rep_timestap { get => rep_timestap; set => rep_timestap = value; }
+    public string Rep_mem_id { get => rep_mem_id; set => rep_mem_id = value; }
+    public string Rep_reason { get => rep_reason; set => rep_reason = value; }
+
+    public override bool Equals(object obj)
+    {
+        var report = obj as Report;
+        return report != null &&
+               rep_no == report.rep_no &&
+               trv_no == report.trv_no &&
+               rep_timestap == report.rep_timestap &&
+               rep_mem_id == report.rep_mem_id &&
+               rep_reason == report.rep_reason;
+    }
+
+    public override int GetHashCode()
+    {
+        var hashCode = -1341125686;
+        hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(rep_no);
+        hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(trv_no);
+        hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(rep_timestap);
+        hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(rep_mem_id);
+        hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(rep_reason);
+        return hashCode;
+    }
+}
--- adminForm/adminForm.cs
+++ adminForm/adminForm.cs
@@ -19,12 +19,11 @@ namespace adminForm
         public adminForm()
         {
             InitializeComponent();
-            InitializeComponent();
             myDB = new MyDB();
             myDB.GetCon();
         }
 
-        public DataTable GetDBTable(string sql)
+        /*public DataTable GetDBTable(string sql)
         {
             MySqlConnection con = myDB.GetCon();
             MySqlDataAdapter adapter = new MySqlDataAdapter(sql, con);
@@ -32,13 +31,28 @@ namespace adminForm
             DataTable dt = new DataTable();
             adapter.Fill(dt);
             return dt;
-        }
+        }*/
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string sql = "SELECT rep_no, trv_no, rep_reason, rep_timestap, rep_mem_id";
-            DataTable dt = GetDBTable(sql);
-            myDB.CloseCon();
+            ReportDao reportdao = new ReportDao();
+            Report report = new Report();
+            Report inputReport = new Report();
+
+            DataSet ds = reportdao.SelectReport();
+            dataGridView1.DataSource = ds.Tables[0];
+            //rep_no, trv_no, rep_timestap, rep_mem_id, rep_reason
+            dataGridView1.Columns[0].HeaderCell.Value = "신고 번호";
+            dataGridView1.Columns[1].HeaderCell.Value = "게시글 번호";
+            dataGridView1.Columns[2].HeaderCell.Value = "신고 일시";
+            dataGridView1.Columns[3].HeaderCell.Value = "신고인";
+            dataGridView1.Columns[4].HeaderCell.Value = "신고 사유";
+
+            //inputReport.Rep_no = "1";
+            //report = reportdao.selectReportByrep_no(inputReport);
+
+            //Console.WriteLine(report.Rep_no);
+
         }
     }
 }
