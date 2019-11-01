00026e4602b02e09b685d5fb0fddf24666ba760b
나평근 <skvudrms54@gmail.com>
2019-11-01 17:11:36 +0900

    MyDB 업로드

---
+++ toourshared/App_Code/Lib/MyDB.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+/// <summary>
+/// MyDB의 요약 설명입니다.
+/// </summary>
+namespace tooushared.Lib
+{
+    public class MyDB
+    {
+        MySqlConnection con = null;
+
+        public MyDB(){
+            String connectionString = "server=itbuddy.iptime.org;user=yuhan;database=toourshared;password=yuhan1234;port=8233;";
+            this.con = new MySqlConnection(connectionString);
+        }
+
+        public MySqlConnection GetCon()
+        {
+
+           
+            return this.con;
+        }
+
+        public void CloseCon()
+        {
+            this.con.Close();
+
+        }
+      
+    }
+}
